9acc46c61ba4f410129acb50d0b21b71daac349e
Paolo valente <paolo.valente@unimore.it>
2012-07-03 08:52:14 +0200

    Bug fix

--- run_multiple_tests/run_all_tests_1.sh
+++ run_multiple_tests/run_all_tests_1.sh
@@ -132,18 +132,17 @@ echo /etc/init.d/cron stop
 rm -rf $RES_DIR
 mkdir -p $RES_DIR
 
-if [ "${NCQ_QUEUE_DEPTH}" != "" ];
-if
-	(echo ${NCQ_QUEUE_DEPTH} > /sys/block/${HD}/device/queue_depth)\
+if [ "${NCQ_QUEUE_DEPTH}" != "" ]; then
+    (echo ${NCQ_QUEUE_DEPTH} > /sys/block/${HD}/device/queue_depth)\
 		 &> /dev/null
-	ret=$?
-	if [[ "$ret" -eq "0" ]]; then
-		echo "Set queue depth to ${NCQ_QUEUE_DEPTH} on ${HD}"
-	elif [[ "$(id -u)" -ne "0" ]]; then
-		echo "You are currently executing this script as $(whoami)."
-		echo "Please run the script as root."
-		exit 1
-	fi
+    ret=$?
+    if [[ "$ret" -eq "0" ]]; then
+	echo "Set queue depth to ${NCQ_QUEUE_DEPTH} on ${HD}"
+    elif [[ "$(id -u)" -ne "0" ]]; then
+	echo "You are currently executing this script as $(whoami)."
+	echo "Please run the script as root."
+	exit 1
+    fi
 fi
 
 for sched in ${schedulers[*]}; do
