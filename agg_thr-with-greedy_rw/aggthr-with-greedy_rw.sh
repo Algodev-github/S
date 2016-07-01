fa4abf4965030e698bcc146bf7c91bf9e0eca7a4
Paolo Valente <paolo.valente@linaro.org>
2016-07-02 00:24:34 +0200

    Improve aggthr-with-greedy_rw.sh
    - Reduce default duration to more reasonable 30 seconds
    - Do not limit write rate by default any more
    - Perform a preliminary sync to wait for the completion of possible
      previous writes before starting new readers/writers
    - Turn into a countdown the simple sleep for waiting for transitory
      to terminate
    - Add countdown during measurement
    - Check that configured duration has been respected. If it has not,
      then declare benchmark failed, because results are not reliable
    
    Signed-off-by: Paolo Valente <paolo.valente@unimore.it>
    Acked-by: Luca Miccio <lucmiccio@gmail.com>

--- agg_thr-with-greedy_rw/aggthr-with-greedy_rw.sh
+++ agg_thr-with-greedy_rw/aggthr-with-greedy_rw.sh
@@ -15,11 +15,13 @@ NUM_READERS=${2-1}
 NUM_WRITERS=${3-0}
 RW_TYPE=${4-seq}
 STAT_DEST_DIR=${5-.}
-DURATION=${6-60}
+DURATION=${6-30}
 SYNC=${7-yes}
-MAXRATE=${8-16500} # maximum value for which the system apparently
-		   # does not risk to become unresponsive under bfq
-		   # with a 90 MB/s hard disk
+MAXRATE=${8-0} # If useful with other schedulers than bfq, 16500
+		   # is apparently the maximum value for which the
+		   # system does not risk to become unresponsive, with
+		   # sequential writers, under any scheduler with a 90
+		   # MB/s hard disk.
 
 # see the following string for usage, or invoke aggthr_of_greedy_rw.sh -h
 usage_msg="\
@@ -56,6 +58,9 @@ rm -f $FILE_TO_WRITE
 
 set_scheduler
 
+echo Preliminary sync to wait for the completion of possible previous writes
+sync
+
 # create and enter work dir
 rm -rf results-${sched}
 mkdir -p results-$sched
@@ -64,40 +69,68 @@ cd results-$sched
 # setup a quick shutdown for Ctrl-C 
 trap "shutdwn 'fio iostat'; exit" sigint
 
-echo Flushing caches
-if [ "$SYNC" != "yes" ]; then
-	echo not syncing
-	echo 3 > /proc/sys/vm/drop_caches
-else
-	echo syncing
-	flush_caches
-fi
-
 init_tracing
 set_tracing 1
 
 start_readers_writers_rw_type $NUM_READERS $NUM_WRITERS $RW_TYPE $MAXRATE
 
-# wait just a little for reader start-up transitory to terminate:
-# we do not want to wait too much, because we want to get
-# also the effects of the transitory
-sleep 2
-
-printf "Reading $NUM_READERS file(s)"
-if [[ $NUM_WRITERS -gt 0 ]]; then
-    printf ", writing $NUM_WRITERS file(s) "
+echo Flushing caches
+if [ "$SYNC" != "yes" ]; then
+	echo Not syncing
+	echo 3 > /proc/sys/vm/drop_caches
 else
-    printf " "
+	# Flushing in parallel, otherwise sync would block for a very
+	# long time
+	flush_caches &
 fi
 
-echo for $DURATION seconds
+if (( $NUM_READERS > 0 || $NUM_WRITERS > 0)); then
+
+	# wait for reader/writer start-up transitory to terminate
+	secs=$(transitory_duration 7)
+
+	while [ $secs -ge 0 ]; do
+	    echo -ne "Waiting for transitory to terminate: $secs\033[0K\r"
+	    sleep 1
+	    : $((secs--))
+	done
+	echo
+fi
+
+echo Measurement started, and lasting $DURATION seconds
+
+start_time=$(date +'%s')
 
 # start logging aggthr
 iostat -tmd /dev/$DEV 2 | tee iostat.out &
 
-sleep $DURATION
+# wait for reader/writer start-up transitory to terminate
+secs=$DURATION
+
+while [ $secs -gt 0 ]; do
+    echo "Remaining time: $secs"
+    if [ "$SYNC" == "yes" ]; then
+	echo Syncing again in parallel ...
+	sync &
+    fi
+    sleep 2
+    : $((secs-=2))
+done
+echo
 
 shutdwn 'fio iostat'
+
+end_time=$(date +'%s')
+
+actual_duration=$(($(date +'%s') - $start_time))
+
+if [ $actual_duration -gt $(($DURATION + 10)) ]; then
+    echo Run lasted $actual_duration seconds instead of $DURATION
+    echo In this conditions the system, and thus the results, are not reliable
+    echo Aborting
+    rm -rf results-${sched}
+    exit
+fi
 
 mkdir -p $STAT_DEST_DIR
 file_name=$STAT_DEST_DIR/\
