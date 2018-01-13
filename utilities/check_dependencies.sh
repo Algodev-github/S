135648a474b12e14bf98b8007df15755edcbcbdb
Paolo Valente <paolo.valente@linaro.org>
2018-01-13 11:14:55 +0100

    check_dependencies: report all failed dependencies at once
    check_dependencies stops, and aborts the whole benchmark, as it finds
    a missing dependecy. This is annoying, because one then discovers the
    next failed dependency only whne retrying to start the benchmark, and
    so on. This commit fixes this problem by making check_dependencies
    check all dependencies at once and report all failed ones.
    
    Signed-off-by: Paolo Valente <paolo.valente@linaro.org>

--- utilities/check_dependencies.sh
+++ utilities/check_dependencies.sh
@@ -6,16 +6,21 @@ function check_dep
     COMMAND_LIST=( "$@" )
     for i in "${COMMAND_LIST[@]}" ; do
    	type $i >/dev/null 2>&1 || \
-	    { echo >&2 "$i is not installed. Aborting..."; \
-	    exit 1; }
+	    { echo >&2 "$i not found."; \
+	    SOME_ABSENT=1; }
     done
+
+    if [ "$SOME_ABSENT" != "" ]; then
+	echo >&2 "Please install above dependencies and retry. Aborting now."
+	exit 1
+    fi
 }
 
 if [[ "$@" == "" ]] ; then
-    echo "Check principal dependencies..."
+    echo "Checking principal dependencies..."
     check_dep awk iostat bc time fio
 
-    echo "Check secondary dependencies..."
+    echo "Checking secondary dependencies..."
     check_dep pv git make
 else
     check_dep "$@"
