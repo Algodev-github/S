bbb239da57f238570d3cb6ab3d66251fe1307329
Paolo Valente <paolo.valente@linaro.org>
2018-01-22 09:37:40 +0100

    config_params: check freshness of user config file
    Signed-off-by: Paolo Valente <paolo.valente@linaro.org>

--- config_params.sh
+++ config_params.sh
@@ -11,6 +11,18 @@ if [ ! -f $CONF_DEST_DIR/.S-config.sh ]; then
 	if [ "$SUDO_USER" != "" ]; then
 		chown $SUDO_USER:$SUDO_USER $CONF_DEST_DIR/.S-config.sh
 	fi
+else
+	if [ ../def_config_params.sh -nt $CONF_DEST_DIR/.S-config.sh ]; then
+		echo Your config file \($CONF_DEST_DIR/.S-config.sh\) is older
+		echo than my default config file. If this is ok for you,
+		echo then just
+		echo touch $CONF_DEST_DIR/.S-config.sh
+		echo to eliminate this error.
+		echo Otherwise
+		echo rm $CONF_DEST_DIR/.S-config.sh
+		echo to have your config file updated automatically.
+		exit
+	fi
 fi
 
 . $CONF_DEST_DIR/.S-config.sh
