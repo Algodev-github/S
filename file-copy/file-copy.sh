556b42af0c52973fb104a31f7f301ba022e7deb6
Quirino Leone <quirinoleone95@gmail.com>
2018-12-11 19:48:33 +0100

    file-copy: improve quality of test by adding extra features
    In this test, many important features are missing.
    
    In particular, there isn't any possibily to remove maxrate during file copy,
    and there are no instructions to calculate execution time.
    
    This commit adds the possibility to execute without a maxrate by replacing
    the previous default value (16500 kB/s) with 0, that now implies an execution
    without a maximum rate.
    In this particular case (maxrate=0), it's also possible to set a new variable
    "$PIPE" with the values "pipe" (default) or "nopipe", in order to execute the
    two dd instructions respectively with and without a pipe.
    It has also been added a time instruction, near the wait, that prints the
    execution time of the dd instructions.

--- file-copy/file-copy.sh
+++ file-copy/file-copy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Copyright (C) 2013 Paolo Valente <paolo.valente@unimore.it>
+# Copyright (C) 2018 Quirino Leone <quirinoleone95@gmail.com>
 
 ../utilities/check_dependencies.sh awk dd fio iostat pv
 if [[ $? -ne 0 ]]; then
@@ -13,23 +14,29 @@ sched=$1
 NUM_COPIERS=${2-1}
 ITERATIONS=${3-10}
 SYNC=${4-yes}
-MAXRATE=${5-16500} # maximum value for which the system apparently
-		   # does not risk to become unresponsive under bfq
-		   # with a 90 MB/s hard disk
+MAXRATE=${5-0} # maximum value for which the system apparently does
+	       # not risk to become unresponsive under bfq with a 90
+	       # MB/s hard disk
+PIPE=${6-pipe}
 
 # see the following string for usage, or invoke file-copy.sh -h
 usage_msg="\
 Usage (as root):\n\
 ./file-copy.sh [\"\" | bfq | cfq | ...] [num_copies] [num_iterations]\n\
-  [max_kB-per-sec]\n\
+  [sync] [max_kB-per-sec] [nopipe]\n\
 \n\
+first parameter equal to \"\" or cur-sched -> do not change scheduler\n\
+sync parameter equal to yes -> invoke sync before starting readers/writers\n\
+max_kB-per-sec parameter equal to 0 -> no limitation on the maxrate\n\
+nopipe parameter set -> execute only one dd command, instead of a pair\n\
+with a pipe in between (only with max_kB-per-sec=0)\n\
 For example:\n\
-sudo ./file-copy.sh bfq 10 3 10000\n\
+sudo ./file-copy.sh bfq 10 3 yes 10000\n\
 switches to bfq and launches, for 3 times, 10 copies in parallel,\n\
 with each copy reading from/writing to a distinct file, at a maximum rate\n\
 equal to 10000 kB/sec.\n\
 \n\
-Default parameter values are \"\", ${NUM_COPIERS}, $ITERATIONS and $MAXRATE\n"
+Default parameter values are \"\", ${NUM_COPIERS}, $ITERATIONS, $SYNC, $MAXRATE and $PIPE\n"
 
 if [ "$1" == "-h" ]; then
         printf "$usage_msg"
@@ -46,7 +53,7 @@ cd results-$sched
 # switch to the desired scheduler
 set_scheduler
 
-# setup a quick shutdown for Ctrl-C 
+# setup a quick shutdown for Ctrl-C
 trap "shutdwn dd; exit" sigint
 
 echo Flushing caches
@@ -70,13 +77,21 @@ do
     # start $NUM_COPIES copiers
     for ((i = 0 ; $i < $NUM_COPIERS ; i++))
     do
-	dd if=${BASE_FILE_PATH}$SUFFIX$i 2>&1 | \
-	    pv -q -L $(($MAXRATE / $NUM_COPIERS))k 2>&1 | dd of=${BASE_FILE_PATH}-copy$i \
-	    > /dev/null 2>&1 &
+	if [[ $MAXRATE -eq 0 && "$PIPE" == "pipe" ]]; then
+	    dd if=${BASE_FILE_PATH}$SUFFIX$i 2>&1 | \
+		dd of=${BASE_FILE_PATH}-copy$i > /dev/null 2>&1 &
+	elif [[ $MAXRATE -eq 0 && "$PIPE" == "nopipe" ]]; then
+	    dd if=${BASE_FILE_PATH}$SUFFIX$i 2>&1 of=${BASE_FILE_PATH}-copy$i \
+						  > /dev/null 2>&1 &
+	else
+	    dd if=${BASE_FILE_PATH}$SUFFIX$i 2>&1 | \
+		pv -q -L $(($MAXRATE / $NUM_COPIERS))k 2>&1 | \
+		dd of=${BASE_FILE_PATH}-copy$i > /dev/null 2>&1 &
+	fi
     done
     echo "Copying $NUM_COPIERS file(s)"
     echo
-    wait
+    time wait
     flush_caches
 done
 
@@ -86,4 +101,3 @@ cd ..
 
 # rm work dir
 rm -rf results-${sched}
-
