ad05f1a396c1699b0d4d0d4e899c37ad40ef8ef0
Paolo Valente <paolo.valente@linaro.org>
2019-08-20 16:26:10 +0200

    bandwidth-latency: turn on weight controller only if needed
    Signed-off-by: Paolo Valente <paolo.valente@linaro.org>

--- bandwidth-latency/bandwidth-latency.sh
+++ bandwidth-latency/bandwidth-latency.sh
@@ -962,14 +962,17 @@ else
 	exit 1
     fi
     echo "+io" > /cgroup/cgroup.subtree_control
-    for dev in $DEVS; do
-	echo "$(cat /sys/block/$dev/dev) enable=1" \
-	     > /cgroup/io.weight.qos
-	if [[ $? -ne 0 ]]; then
-	    echo Failed to enable weight controller
-	    exit 1
-	fi
-    done
+
+    if [[ "$type_bw_control" == weight ]]; then
+	for dev in $DEVS; do
+	    echo "$(cat /sys/block/$dev/dev) enable=1" \
+		 > /cgroup/io.weight.qos
+	    if [[ $? -ne 0 ]]; then
+		echo Failed to enable weight controller for $dev
+		exit 1
+	    fi
+	done
+    fi
 fi
 
 for ((i = 0 ; $i < $num_groups ; i++)) ; do
