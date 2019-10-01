914a9840a4c899a84a6a2528c1e6c619e29f3668
Paolo Valente <paolo.valente@linaro.org>
2019-10-01 18:42:14 +0200

    add create_config.sh
    Signed-off-by: Paolo Valente <paolo.valente@linaro.org>

--- config_params.sh
+++ config_params.sh
@@ -1,35 +1,3 @@
-if [ "$SUDO_USER" != "" ]; then
-    eval CONF_DEST_DIR=~$SUDO_USER
-else
-    CONF_DEST_DIR=~
-fi
-
-if [ ! -f $CONF_DEST_DIR/.S-config.sh ]; then
-	echo No user config found in $CONF_DEST_DIR, copying default config
-	tail -n +5 ../def_config.sh > $CONF_DEST_DIR/.S-config.sh
-
-	if [ "$SUDO_USER" != "" ]; then
-		chown $SUDO_USER:$SUDO_USER $CONF_DEST_DIR/.S-config.sh
-	fi
-else
-	sed 's/^#.*//g' ../def_config.sh > def_file
-	sed 's/^#.*//g' $CONF_DEST_DIR/.S-config.sh > user_file
-	if [[ "$(diff -d -B def_file user_file)" != "" && \
-		  ../def_config.sh -nt $CONF_DEST_DIR/.S-config.sh ]]; then
-		echo Your config file \($CONF_DEST_DIR/.S-config.sh\) is older
-		echo than my default config file. If this is ok for you,
-		echo then just
-		echo touch $CONF_DEST_DIR/.S-config.sh
-		echo to eliminate this error.
-		echo Otherwise
-		echo rm $CONF_DEST_DIR/.S-config.sh
-		echo to have your config file updated automatically
-		echo with default values.
-		rm def_file user_file
-		exit
-	fi
-	rm def_file user_file
-fi
-
+. ../create_config.sh
 . $CONF_DEST_DIR/.S-config.sh
 . ../process_config.sh
---
+++ create_config.sh
@@ -0,0 +1,39 @@
+if [ "$SUDO_USER" != "" ]; then
+    eval CONF_DEST_DIR=~$SUDO_USER
+else
+    CONF_DEST_DIR=~
+fi
+
+if [ -f def_config.sh ]; then
+    ROOTDIR=$(pwd)
+else
+    ROOTDIR=$(pwd)/..
+fi
+
+if [ ! -f $CONF_DEST_DIR/.S-config.sh ]; then
+	echo No user config found in $CONF_DEST_DIR, copying default config
+	tail -n +5 $ROOTDIR/def_config.sh > $CONF_DEST_DIR/.S-config.sh
+
+	if [ "$SUDO_USER" != "" ]; then
+		chown $SUDO_USER:$SUDO_USER $CONF_DEST_DIR/.S-config.sh
+	fi
+else
+	sed 's/^#.*//g' $ROOTDIR/def_config.sh > def_file
+	sed 's/^#.*//g' $CONF_DEST_DIR/.S-config.sh > user_file
+	if [[ "$(diff -d -B def_file user_file)" != "" && \
+		  $ROOTDIR/def_config.sh -nt $CONF_DEST_DIR/.S-config.sh ]]
+	then
+		echo Your config file \($CONF_DEST_DIR/.S-config.sh\) is older
+		echo than my default config file. If this is ok for you,
+		echo then just
+		echo touch $CONF_DEST_DIR/.S-config.sh
+		echo to eliminate this error.
+		echo Otherwise
+		echo rm $CONF_DEST_DIR/.S-config.sh
+		echo to have your config file updated automatically
+		echo with default values.
+		rm def_file user_file
+		exit
+	fi
+	rm def_file user_file
+fi
