73a0fb20a340b336e3fea7dcac0ff252a48ba91e
localstepdo <localstepdo@x1stepdo3.corp.microsoft.com>
2020-04-27 20:29:30 -0700

    live os partitions don't always contain a slash

--- process_config.sh
+++ process_config.sh
@@ -39,31 +39,8 @@ function print_dev_help
 
 function find_partition_for_dir
 {
-    PART=
-    longest_substr=
-    mount |
-	{ while IFS= read -r var
-	  do
-	      curpart=$(echo "$var" | cut -f 1 -d " ")
-
-	      if [[ "$(echo $curpart | egrep '/')" == "" ]]; then
-		  continue
-	      fi
-
-	      mountpoint=$(echo "$var" | \
-			       sed 's<.* on \(.*\)<\1<' | \
-			       sed 's<\(.*\) type.*<\1<')
-	      substr=$(printf "%s\n%s\n" "$mountpoint" "$1" | \
-			   sed -e 'N;s/^\(.*\).*\n\1.*$/\1/')
-
-	      if [[ "$substr" == $mountpoint && \
-			${#substr} -gt ${#longest_substr} ]] ; then
-		  longest_substr=$substr
-		  PART=$(echo "$var" | cut -f 1 -d " ")
-	      fi
-	  done
-	  echo $PART
-	}
+	PART=$(df "$1" | tail -1 | awk '{print $1;}')
+	echo $PART
 }
 
 function find_dev_for_dir
