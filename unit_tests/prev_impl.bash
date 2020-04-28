70609749f7a74bf46dd04d1e564005b138831bdd
localstepdo <localstepdo@x1stepdo3.corp.microsoft.com>
2020-04-27 20:37:25 -0700

    introduce automated tests

---
+++ unit_tests/prev_impl.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# previous implementations
+
+function original_find_partition_for_dir
+{
+    PART=
+    longest_substr=
+    mount |
+    {
+        while IFS= read -r var
+        do
+            curpart=$(echo "$var" | cut -f 1 -d " ")
+
+            if [[ "$(echo $curpart | egrep '/')" == "" ]] && [[ -z "$2" ]]; then
+                continue
+            fi
+
+            mountpoint=$(echo "$var" | \
+                            sed 's<.* on \(.*\)<\1<' | \
+                            sed 's<\(.*\) type.*<\1<')
+            substr=$(printf "%s\n%s\n" "$mountpoint" "$1" | \
+                        sed -e 'N;s/^\(.*\).*\n\1.*$/\1/')
+
+            if [[ "$substr" == $mountpoint && \
+                    ${#substr} -gt ${#longest_substr} ]] ; then
+                longest_substr=$substr
+                PART=$(echo "$var" | cut -f 1 -d " ")
+            fi
+        done
+        echo $PART
+    }
+}
